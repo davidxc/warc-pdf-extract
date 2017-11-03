21485bd31a9f8c6a55da81b1affb9d4b6f602f56
Bryan Newbold <bnewbold@archive.org>
2017-11-03 15:15:31 -0700

    grobid2json.py (and tests)

---
+++ test/test_grobid2json.py
@@ -0,0 +1,81 @@
+
+from nose.tools import assert_equals
+from bin.grobid2json import *
+
+
+class TestConvert:
+
+    def setup(self):
+        self.test_file = "test/files/faca7e910d43d72609d32fb910a64623a6cee8e4.tei.xml"
+
+    def test_header(self):
+        extracted = do_tei(self.test_file)
+        info = dict()
+        info['title'] = "Exact and stable recovery of rotations for robust synchronization"
+        info['authors'] = [
+            dict(name="L Wang"),
+            dict(name="A Singer"),
+        ]
+        info['journal'] = dict(
+            name="Information and Inference",
+            issn="2049-8764",
+            eissn="2049-8772",
+            publisher="Oxford University Press (OUP)",
+            volume="2",
+            issue="2")
+        info['date'] = "2013"
+        info['doi'] = "10.1093/imaiai/iat005"
+
+        for field in ['title', 'authors', 'journal', 'date', 'doi']:
+            assert_equals(info[field], extracted[field])
+
+    def test_refs(self):
+        extracted = do_tei(self.test_file)
+        ref0 = dict()
+        ref19 = dict()
+        ref_count = 46
+
+        ref0['index'] = 0
+        ref0['id'] = 'b0'
+        ref0['title'] = "On the concentration of eigenvalues of random symmetric matrices"
+        ref0['authors'] = [
+            dict(name="N Alon"),
+            dict(name="M Krivelevich"),
+            dict(name="V Vu"),
+        ]
+        ref0['journal'] = "Israel J. Math"
+        ref0['publisher'] = None
+        ref0['volume'] = "131"
+        ref0['date'] = "2002"
+        ref0['issue'] = None
+        ref0['url'] = None
+
+        ref19['index'] = 19
+        ref19['id'] = 'b19'
+        ref19['title'] = "Estimation and registration on graphs"
+        ref19['authors'] = [
+            dict(name="S Howard"),
+            dict(name="D Cochran"),
+            dict(name="W Moran"),
+            dict(name="F Cohen"),
+        ]
+        ref19['journal'] = None
+        ref19['publisher'] = None
+        ref19['volume'] = None
+        ref19['date'] = "2010-08-05"
+        ref19['issue'] = None
+        ref19['url'] = "http://arxiv.org/abs/1010.2983"
+
+        assert_equals(ref0, extracted['citations'][0])
+        assert_equals(ref19, extracted['citations'][19])
+
+    def test_fulltext(self):
+        extracted = do_tei(self.test_file)
+
+        ack_snip = "The authors thank Zaiwen Wen for many useful discussions regarding the ADM"
+        abstract_snip = "The synchronization problem over the special orthogonal group SO(d)"
+        body_snip = "The rotations obtained in this manner are uniquely determined up to a global rotation"
+
+        assert ack_snip in extracted['acknowledgement']
+        assert abstract_snip in extracted['abstract']
+        assert body_snip in extracted['body']
